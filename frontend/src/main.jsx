92ce1667417ec40baaa4bd734f0f3a95fdfa746c
David Kempkens <david.kempkens@gmail.com>
2025-03-08 12:37:26 +0100

    feat: add ProfileScreen and PrivateRoute for user profile management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import PrivateRoute from "./components/PrivateRoute";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import store from "./store";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
@@ -19,6 +21,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Route index path="/" element={<HomeScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
+            <Route path="" element={<PrivateRoute />}>
+              <Route path="/profile" element={<ProfileScreen />} />
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
