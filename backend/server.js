d504775c39bad32afc0b4e040f6ba01ea016651c
David Kempkens <david.kempkens@gmail.com>
2025-03-08 12:46:13 +0100

    feat: serve frontend files in production and fallback to API message in development

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
@@ -20,9 +21,18 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.use(notFound);
 app.use(errorHandler);
